bf6ea7e1a9e368b65552cf113fb892ae89ba6e69
tweber <tweber@xevo.com>
2019-08-16 18:08:36 -0700

    Refactored application to use vailla Redux.

--- src/reducers/items-reducer.js
+++ src/reducers/items-reducer.js
@@ -1,5 +1,21 @@
 import { ADD_NEW_ITEM, REMOVE_ITEM, TOGGLE_ITEM, MARK_ALL_AS_UNPACKED } from '../constants';
 
 export default function(state = {}, action) {
+  if (action.type === ADD_NEW_ITEM) {
+    return [ ...state, action.item ]
+  }
+
+  if (action.type === REMOVE_ITEM) {
+    return state.filter(item => item.id !== action.id)
+  }
+
+  if (action.type === TOGGLE_ITEM) {
+    const item = action.item;
+    return [ ...state, !item.packed]
+  }
+
+  if (action.type === MARK_ALL_AS_UNPACKED) {
+    return [ ...state, action.item.packed = false]
+  }
   return state;
 }
